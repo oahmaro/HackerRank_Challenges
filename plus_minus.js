31b9cf661f6a7584b0d3ed88c57177650b818f8a
oahmaro <osamaahmaro@gmail.com>
2019-04-18 22:51:25 +0300

    Solution 01

---
+++ plus_minus.js
@@ -0,0 +1,27 @@
+function plusMinus(arr) {
+  let positiveCounter = 0
+  let negativeCounter = 0
+  let zeroCounter = 0
+
+  arr.forEach((item) => {
+    if (item === 0) {
+      zeroCounter += 1
+    }
+    if (item > 0) {
+      positiveCounter += 1
+    }
+    if (item < 0) {
+      negativeCounter += 1
+    }
+  })
+
+  console.log(positiveCounter / arr.length)
+  console.log(negativeCounter / arr.length)
+  console.log(zeroCounter / arr.length)
+}
+
+// plusMinus([-4, 3, -9, 0, 4, 1])
+// Expected Result:
+// 0.500000
+// 0.333333
+// 0.166667
