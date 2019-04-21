3a1a6cea0e7dcf0f6e46abdbc43c0f950855f87f
oahmaro <osamaahmaro@gmail.com>
2019-04-21 16:41:06 +0300

    Solution 01

---
+++ minMaxSum.js
@@ -0,0 +1,21 @@
+function miniMaxSum(arr) {
+  let sum = 0
+  arr.sort().forEach((item) => {
+    sum += item
+  })
+
+  let minSum = sum - arr[4]
+  let maxSum = sum - arr[0]
+
+  console.log(`${minSum} ${maxSum}`)
+}
+
+// Sample Input 1:
+// [1, 3, 5, 7, 9]
+// Sample Output 1:
+// 16 24
+
+// Sample Input 2:
+// [1, 2, 3, 4, 5]
+// Sample Output 2:
+// 10 14
