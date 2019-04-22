2c3acf8cdbfc638b857be79496d1d3d2875e1ae3
oahmaro <osamaahmaro@gmail.com>
2019-04-22 23:43:58 +0300

    Solution 01

---
+++ appleAndOrange.js
@@ -0,0 +1,28 @@
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+  let applesFalls = 0
+  let orangesFalls = 0
+  let limiter = apples.length > oranges.length ? apples.length : oranges.length
+  for (let i = 0; i < limiter; i++) {
+    if(apples[i] !== undefined) {
+      let applePos = a + apples[i]
+      if (applePos >= s && applePos <= t) {
+        applesFalls += 1
+      }
+    }
+    if(oranges[i] !== undefined) {
+      let orangePos = b + oranges[i]
+      if (orangePos >= s && orangePos <= t) {
+        orangesFalls += 1
+      }
+    }
+  }
+  console.log(applesFalls + '\n' + orangesFalls)
+}
+
+// Sample Input 1: 
+// 7, 10, 4, 12, [2, 3, -4], [3, -2, -4]
+// Sample Output1:
+// 1
+// 2
+
+
