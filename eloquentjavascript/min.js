f6b2c2beef25b15f527b93571748f2bc3842ee32
oahmaro <osamaahmaro@gmail.com>
2019-04-23 11:41:37 +0300

    Solution 01

---
+++ eloquentjavascript/min.js
@@ -0,0 +1,13 @@
+/* Write a function
+min that takes two arguments and returns their minimum.
+*/
+function min(a, b) {
+  if (a < b) {
+    return a
+  } return b
+}
+
+// Sample Input:
+// 3 5
+// Sample Output:
+// 3
