bf8eadfeaf114370f8725e4dcaa671c1d7836d20
oahmaro <osamaahmaro@gmail.com>
2019-04-23 11:29:11 +0300

    Solution 01

---
+++ eloquentjavascript/loopingATriangle.js
@@ -0,0 +1,20 @@
+/* Write a loop that makes sven calls to console.log
+ to output the following triangle:
+#
+##
+###
+####
+#####
+######
+#######
+*/
+
+function loopingTriangle() {
+  for (let i = 1; i <= 7; i++) {
+    let line = ''
+    for (let j = 1; j <= i; j++) {
+      line += '*'
+    }
+    console.log(line)
+  }
+}
