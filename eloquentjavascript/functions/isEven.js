5f2e3412f3e65d38a8c5591463048c28d8745d03
oahmaro <osamaahmaro@gmail.com>
2019-04-23 12:41:58 +0300

    Solution 02

--- eloquentjavascript/functions/isEven.js
+++ eloquentjavascript/functions/isEven.js
@@ -31,3 +31,11 @@ function isEven(n) {
 // -77
 // Sample Output 3:
 // false
+
+// Better Solution
+function isEven(n) {
+  if (n == 0) return true
+  else if (n === 1) return false
+  else if (n < 0) return isEven(-n)
+  else return isEven(n - 2) 
+}
