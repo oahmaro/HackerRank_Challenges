3ffa097297c63ac29aea1adc40d9f40cf7d17684
oahmaro <osamaahmaro@gmail.com>
2019-04-23 12:26:49 +0300

    Soultion 02

--- eloquentjavascript/fizzBuzz.js
+++ eloquentjavascript/fizzBuzz.js
@@ -16,3 +16,13 @@ function fizzBuzz() {
     } else console.log(i)
   }
 }
+
+// Better Solution
+function fizzBuzz() {
+  for (let i = 1; i <= 100; i++) {
+    let output = ''
+    if (n % 3 === 0) output += 'Fizz'
+    if (n % 5 === 0) output += 'Buzz'
+    console.log(output || n)
+  }
+}
